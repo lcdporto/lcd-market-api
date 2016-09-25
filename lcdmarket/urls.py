cd6f45cdfc948fc0ac9669cbb5368f5b05ac3a33
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-09-25 13:31:28 +0100

    removes unused nested router dependency

--- lcdmarket/urls.py
+++ lcdmarket/urls.py
@@ -8,7 +8,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-from rest_framework_nested import routers
 from rest_framework import routers
 
 from lcdmarket.api import views
